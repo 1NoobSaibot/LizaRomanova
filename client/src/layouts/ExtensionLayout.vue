479e3185fe5674cb1df59717093ade7666bb0651
Zheka Diulin <zheka.diulin@gmail.com>
2020-12-15 16:36:43 +0200

    [client] Кнопка, которая открывает новую вкладку с админкой

--- client/src/layouts/ExtensionLayout.vue
+++ client/src/layouts/ExtensionLayout.vue
@@ -13,8 +13,9 @@
               </q-item-label>
             </q-item-section>
           </q-item>
-          <q-btn @click="logout" flat dense round icon="logout"/>
+          <q-btn @click="logout" title="Выход" flat dense round icon="logout"/>
         </div>
+        <q-btn @click="openAdminTab" title="Перейти на сайт" flat dense round icon="tab"/>
       </q-toolbar>
     </q-header>
     <q-page-container>
@@ -29,6 +30,11 @@ export default {
   methods: {
     async logout () {
       await this.$store.dispatch('admin/logout')
+    },
+    openAdminTab () {
+      chrome.tabs.create({
+        url: 'http://localhost:8080/admin/login'
+      })
     }
   }
 }
